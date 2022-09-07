b24f518c6ad640346e7b3fb0e1c6352ecaf71dd3
Amir Moris <amir.moris35@gmail.com>
2022-09-07 03:21:39 +0200

    feat: displaying lectures in a slider

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-let activeTab = "Python";
+// course carousel
+let carouselWidth = document.querySelector(".carousel-container").offsetWidth;
+const next_btn = document.querySelector(".carousel-buttons .next");
+const prev_btn = document.querySelector(".carousel-buttons .prev");
+const track = document.querySelector(".carousel-container .track");
+// if the screen size changed
+window.addEventListener("resize", () => {
+  carouselWidth = document.querySelector(".carousel-container").offsetWidth;
+});
+
+// buttons
+let index = 0;
+next_btn.addEventListener("click", () => {
+  index++;
+  track.style.transform = `translateX(-${index * carouselWidth}px)`;
+  prev_btn.style.display = "block";
+  console.log(track.offsetWidth, index, index * carouselWidth, carouselWidth);
+  if (track.offsetWidth - index * carouselWidth < carouselWidth) {
+    next_btn.style.display = "none";
+  }
+});
+
+prev_btn.addEventListener("click", () => {
+  index--;
+  track.style.transform = `translateX(-${index * carouselWidth}px)`;
+  next_btn.style.display = "block";
+
+  if (index === 0) {
+    prev_btn.style.display = "none";
+  }
+});
+
 // course lectures
+let activeTab = "Python";
 async function generateCard(lecture) {
   // create lectureCard
   let card = document.createElement("div");
   card.className = "lectureCard";
   // lectureCard image
-  let courseImg = document.createElement("img");
-  courseImg.src = lecture["image"];
-  courseImg.alt = "course image";
+  let courseImg = document.createElement("div");
+  courseImg.className = "courseImg";
+  myImage = document.createElement("img");
+  myImage.src = lecture["image"];
+  myImage.alt = "course image";
+  courseImg.appendChild(myImage);
   card.appendChild(courseImg);
   // lectureCard title
   let title = document.createElement("h6");
@@ -30,11 +65,11 @@ async function generateCard(lecture) {
   for (let i = 1, myrating = lecture["rating"]; i <= 5; i++, myrating--) {
     let star = document.createElement("span");
     if (myrating >= 1) {
-      star.className = "fa fa-star checked";
+      star.className = "bi bi-star-fill";
     } else if (myrating > 0) {
-      star.className = "fa fa-star-half-full";
+      star.className = "bi bi-star";
     } else {
-      star.className = "fa fa-star-o";
+      star.className = "bi bi-star-half";
     }
     rating.appendChild(star);
   }
@@ -57,7 +92,10 @@ async function generateCard(lecture) {
     price.appendChild(price_without_offer);
   }
   card.appendChild(price);
-  document.querySelector(".card-wrapper").appendChild(card);
+  const temp = document.createElement("div");
+  temp.className = "card-container";
+  temp.appendChild(card);
+  document.querySelector(".track").appendChild(temp);
 }
 const fetchCourse = fetch("http://localhost:3000/" + activeTab).then(
   (response) => {
@@ -81,7 +119,7 @@ const searchInput = document.querySelector(".search-bar input");
 const searchButton = document.querySelector(".search-bar button");
 function searchData(event) {
   if (event.key !== "Enter" && event != "Enter") return;
-  // get lectures` data
+  // get lectures data
   const searchFetch = fetch("http://localhost:3000/" + activeTab)
     .then((searchResult) => {
       return searchResult.json();
@@ -101,7 +139,7 @@ function searchData(event) {
         let title = compareStrings(lecture["title"], searchInput.value);
         let author = compareStrings(lecture["author"], searchInput.value);
         let price = compareStrings(lecture["price"], searchInput.value);
-        console.log(title, author, price);
+        // console.log(title, author, price);
         if (title === false && author === false && price === false) {
           let temp = document.querySelectorAll(".container .lectureCard")[
             lecture["id"]
